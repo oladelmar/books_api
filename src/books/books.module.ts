34c5a02bb1c1c92054929cbf93b93c7a606aa4a5
Oladelmar <mashasbc@gmail.com>
2020-05-24 13:14:58 +0300

    Check author exists at book create and book update

--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -3,9 +3,10 @@ import { BooksController } from './books.controller';
 import { BooksService } from './books.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BookRepository } from './book.repository';
+import { AuthorRepository } from 'src/authors/author.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([BookRepository])],
+  imports: [TypeOrmModule.forFeature([BookRepository, AuthorRepository])],
   controllers: [BooksController],
   providers: [BooksService],
 })
--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -9,12 +9,15 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Book } from './book.entity';
 import { GetBooksFilterDto } from './dto/get-books-filter.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
+import { AuthorRepository } from '../authors/author.repository';
 
 @Injectable()
 export class BooksService {
   constructor(
     @InjectRepository(BookRepository)
+    @InjectRepository(AuthorRepository)
     private bookRepository: BookRepository,
+    private authorRepository: AuthorRepository,
   ) {}
 
   async getBooks(filterDto: GetBooksFilterDto): Promise<Book[]> {
@@ -30,12 +33,26 @@ export class BooksService {
   }
 
   async createBook(createBookDto: CreateBookDto): Promise<Book> {
+    const author = await this.authorRepository.findOne(createBookDto.author);
+    if (!author) {
+      throw new NotFoundException(
+        `Author with id "${createBookDto.author}" not found`,
+      );
+    }
     return this.bookRepository.createBook(createBookDto);
   }
 
   async updateBook(id: string, updateBookDto: UpdateBookDto): Promise<void> {
     if (!Object.entries(updateBookDto).length) {
       throw new BadRequestException('No data provided for update');
+    }
+    if (updateBookDto.author) {
+      const author = await this.authorRepository.findOne(updateBookDto.author);
+      if (!author) {
+        throw new NotFoundException(
+          `Author with id "${updateBookDto.author}" not found`,
+        );
+      }
     }
     await this.getBookById(id);
     await this.bookRepository.updateBook(id, updateBookDto);
